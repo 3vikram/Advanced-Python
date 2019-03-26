f8471308bef80734c227d48a0415e54072273d0e
3vikram <viku.vikram05@gmail.com>
2019-03-26 22:03:12 +0530

    Update lambdaMap.py

--- Lambda function/lambdaMap.py
+++ Lambda function/lambdaMap.py
@@ -1,6 +1,9 @@
 num_list = [1,2,3,4,5,6,7,8,9]
 sqrt = list(map(lambda num: num*num, num_list))
 
+// map takes function and a list or inputs
+// map(func, list or user_input)
+
 print(sqrt)
 
 Result:
